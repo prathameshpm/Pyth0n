51abdd7c3c675d2f8c7abaaf76145812604cf9f3
prathameshpm <mahamulkarprathamesh@gmail.com>
2023-10-29 23:27:52 +0530

    cs50p func and var

---
+++ cs50p/01_functions_variables/hello.py
@@ -0,0 +1,65 @@
+#print("hello, world")
+
+#input("What's your name? ")
+#print("hello, world")
+
+#input("What's your name? ")
+#print("hello, David")
+
+#name = input("What's your name? ")
+#print("hello, name")
+
+#name = input("What's your name? ")
+#print("hello,")
+#print(name)
+
+#name = input("What's your name? ")
+#print("hello, " + name)
+
+#name = input("What's your name? ")
+#print("hello, " + name)
+
+#name = input("What's your name? ")
+#print("hello,", name)
+
+#name = input("What's your name? ")
+#print("hello, ", end="")
+#print(name)
+
+#name = input("What's your name? ")
+#print("hello, ", end="???")
+#print(name)
+
+#name = input("What's your name? ")
+#print("hello, ", name, sep="")
+
+#print('hello, "friend"')
+
+#print("hello, \"friend\"")
+
+#name = input("What's your name? ")
+#print("hello, {name}")
+
+#name = input("What's your name? ")
+#print(f"hello, {name}")
+
+#name = input("What's your name? ")
+#name = name.strip()
+#print(f"hello, {name}")
+
+#name = input("What's your name? ")
+#name = name.strip()
+#name = name.capitalize()
+#print(f"hello, {name}")
+
+#name = input("What's your name? ")
+#name = name.strip()
+#name = name.title()
+#print(f"hello, {name}")
+
+#name = input("What's your name? ")
+#name = name.strip().title()
+#print(f"hello, {name}")
+
+name = input("What's your name? ").strip().title()
+print(f"hello, {name}")
