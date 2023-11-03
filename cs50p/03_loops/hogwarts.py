05cb6fdbd23efc2ec6eccd52138037bee722c06c
prathameshpm <mahamulkarprathamesh@gmail.com>
2023-11-03 17:43:53 +0530

    loops

--- cs50p/03_loops/hogwarts.py
+++ cs50p/03_loops/hogwarts.py
@@ -27,6 +27,7 @@ for i in range(len(students)):
     print(i + 1, students[i])
 """
 
+"""
 students = {
     "Hermione": "Gryffindor",
     "Harry": "Gryffindor",
@@ -38,3 +39,64 @@ print(students["Hermione"])
 print(students["Harry"])
 print(students["Ron"])
 print(students["Draco"])
+"""
+
+"""
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron":  "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student)
+"""
+
+"""
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron":  "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student, students[student])
+"""
+
+"""
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron":  "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
+"""
+
+
+students = [
+    {   "name": "Hermione",
+        "house": "Gryffindor",
+        "patronus": "Otter"
+    },
+    {   "name": "Harry",
+        "house": "Gryffindor",
+        "patronus": "Stag"
+    },
+    {   "name": "Ron",
+        "house": "Gryffindor",
+        "patronus": "Jack Russell Terrier"
+    },
+    {   "name": "Draco",
+        "house": "Slytherin",
+        "patronus": None
+    }
+]
+
+for student in students:
+    print(student["name"], student["house"], 
+    student["patronus"], sep=", ")
